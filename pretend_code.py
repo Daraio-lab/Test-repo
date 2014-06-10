390bead38b3465e09a2f77e3ab65028539a1e124
Paul Anzel <anzelpwj@gmail.com>
2014-06-10 17:44:13 +0200

    Text string
    Just to make it less confusing

--- pretend_code.py
+++ pretend_code.py
@@ -1,6 +1,11 @@
+"""
+Here's a plot thing
+"""
+
 print "Something something"
 x = sin(t)
 y = Advanced_Wavelet_Function(x)
 
 fy = Haar_Wavelet_Function(x)
 # This sill never work, so ein Mist...
+
